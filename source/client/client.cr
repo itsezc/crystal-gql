a567ec63206238727c9762e4001cd17be49ec0ea
itzezc <chirub@foretag.co>
2020-12-18 04:53:58 +0530

    useQuery

---
+++ source/client/client.cr
@@ -0,0 +1,67 @@
+require "json"
+require "http/client"
+
+class GraphQLClient 
+	def initialize(endpoint : String)
+		protected @endpoint = endpoint
+		@loading = false
+	end
+
+	def request(data)
+		@loading = true
+		headers = HTTP::Headers {
+			"accept" => "application/json",
+			"content-type" => "application/json"
+		}
+		response = JSON.parse(
+			HTTP::Client
+				.post(
+					@endpoint, 
+					headers: headers,
+					body:  data
+				)
+				.body
+			)
+		@loading = false
+		return response
+	end
+
+	def query(query : String, variables = {} of Symbol => String)
+		requestQuery = JSON.build do | json |
+			json.object do
+				json.field "query", query
+				json.field "variables", variables
+			end
+		end
+		request(requestQuery)
+	end
+
+	def useQuery(hash : Hash(String, Array(Hash(String, Array(String)) | String)), variables = {} of Symbol => String)
+		query = %{
+			{
+				#{hash.first_key} {
+					#{
+						hash.first_value
+							.join("\n")
+							.gsub("{", "")
+							.gsub("}", "")
+							.gsub("=>", "")
+							.gsub("[", "{")
+							.gsub("]", "}")
+							.gsub("\"", "")
+							.gsub(",",  "\n")
+					}
+				}
+			}
+		}
+
+		requestQuery = JSON.build do | json |
+			json.object do 
+				json.field "query", query
+				json.field "variables", variables
+			end
+		end
+
+		request(requestQuery)
+	end
+end
